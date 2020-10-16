4c874c2c4afef1bead0a62746277d375d973eb40
SaraBrazille <brazillesara@gmail.com>
2020-10-16 18:46:40 -0400

    fixed banner

--- src/pages/PopUps/PopUps.js
+++ src/pages/PopUps/PopUps.js
@@ -15,42 +15,54 @@ const styles = theme => ({
 });
 
 class Home extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            bannerIndex: 0,
+            MyBanners: new Array('banner1.jpg', 'banner2.jpg', 'banner3.jpg', 'banner4.jpg')
+        }
+    }
+
+    componentDidMount(){
+        setTimeout(this.ShowBanners, 5000)
+    }
+
+
+    ShowBanners = () => {
+        if (document.images) {
+            let banner = this.state.bannerIndex + 1;
+            if (banner == this.state.MyBanners.length) {
+                banner = 0
+            }
+            this.setState({
+                bannerIndex: banner
+            })
+            setTimeout(this.ShowBanners, 5000)
+        }
+    }
+
     render() {
         const { classes } = this.props;
 
+        let curBanner = this.state.MyBanners[this.state.bannerIndex];
+
         return (
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <Menu />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        Free Money
+            <>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                            <Menu />
+                        </IconButton>s
+                        <Typography variant="h6" className={classes.title}>
+                            Free Money
 
-                </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-            </AppBar>
-
-            <html>
-             language="Javascript">MyBanners=new Array('banner1.jpg','banner2.jpg','banner3.jpg','banner4.jpg')
-            banner=0
-            function ShowBanners()
-        { if (document.images)
-        {banner++
-        if (banner==MyBanners.length) {
-                    banner = 0}
-        document.ChangeBanner.src=MyBanners[banner]
-        setTimeout("ShowBanners()",5000)
-        }
-        }
-        
-            <body onload="ShowBanners()">
-                <center>
-                    <img src="banner1.jpg" width="900" height="120" name="ChangeBanner" />
-                </center>
-            </body>
-        </html> 
+                    </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+                <img src={"/" + curBanner} width="900" height="120" name="ChangeBanner" />
+            </>
         )
     }
 }
