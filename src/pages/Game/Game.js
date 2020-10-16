9746ed8aaa6e2a9588ee13bf84dde663fb4f44f7
SaraBrazille <brazillesara@gmail.com>
2020-10-16 18:37:43 -0400

    More game

--- src/pages/Game/Game.js
+++ src/pages/Game/Game.js
@@ -40,6 +40,10 @@ const styles = theme => ({
         left: 0,
         right: 0,
         backgroundImage: "url('/blood.jpg')"
+    },
+    restartButton:{
+        display: "flex",
+        justifyContent: "flex-end"
     }
 });
 
@@ -116,7 +120,7 @@ class Game extends Component {
     }
 
     handleRestart = () => {
-
+        this.componentDidMount();
     }
 
     render() {
@@ -144,7 +148,7 @@ class Game extends Component {
                                     WPM: {this.state.wpm}
                                 </Typography>
                             </Grid>
-                            <Grid item xs={6}>
+                            <Grid className = {classes.restartButton} item xs={6}>
                                 <Button className={classes.restartButton} variant="contained" color="primary" onClick={this.handleRestart}>
                                     Restart
                                 </Button>
