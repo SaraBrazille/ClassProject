c6b6bcf27d134bc0c4fa20c75b102edf984f34ad
Spotexx <travesimo@gmail.com>
2020-10-20 18:48:00 -0400

    merged

--- src/components/ChatBot.js
+++ src/components/ChatBot.js
@@ -9,7 +9,6 @@ const styles = theme => ({
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-<<<<<<< HEAD
         border: "solid 3px " + theme.palette.secondary.main,
         background: theme.palette.primary.main,
         display: 'flex'
@@ -22,14 +21,12 @@ const styles = theme => ({
         height: "40px",
         alignContent: "flex-end",
         width: "100%",
-        wordBreak: "break-all"
-=======
+        wordBreak: "break-all",
         border: "solid 3px " + theme.palette.primary.main,
         background: "#f0f0f0",
         position: "absolute !important",
         bottom: 0,
         right: 0
->>>>>>> 91649dc17364e307b1feca8de7dc9e4f8de5d7d6
     }
 });
 
@@ -45,15 +42,11 @@ class ChatBot extends Component {
                     height: 200
                 }}
             >
-<<<<<<< HEAD
                 <div className={classes.bodyBox}>
                     randomtesting
                 </div>
                 <input type="text" className={classes.textBox} value="test"/>
                 
-=======
-                <Button fullWidth variant="contained" color="primary">Test Button</Button>
->>>>>>> 91649dc17364e307b1feca8de7dc9e4f8de5d7d6
             </Resizable>
         )
     }
